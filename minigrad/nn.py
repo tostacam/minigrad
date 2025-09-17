5d0b4bca70e15e1e9d47ab1080cb22bbcb239858
tostacam <edgar-tostado@hotmail.com>
2025-09-16 17:11:04 -0700

    added __repr__, nn all linear for now

--- minigrad/nn.py
+++ minigrad/nn.py
@@ -2,26 +2,33 @@ import random
 from .engine import Value
 
 class Neuron:
-  def __init__(self, nin):
-    self.w = [Value(random.uniform(-1,1)) for i in range(nin)]
-    self.b = Value(random.uniform(-1,1))
+  def __init__(self, nin, linear=True):
+    self.w = [Value(random.uniform(-1,1), label='w') for _ in range(nin)]
+    self.b = Value(random.uniform(-1,1), label='b')
+    self.linear = linear
 
   def __call__(self, x):
     # w * x + b
-    act = sum(wi * xi for wi, xi in zip(self.w, x)) + self.b
-    out = act.tanh()
-    return out
+    act = sum([wi * xi for wi, xi in zip(self.w, x)], start=self.b)
+    act.label = 'neuron(xi): ' + ' '.join(map(str,x))
+    return act if self.linear else act.relu()
+  
+  def __repr__(self):
+    return f"Neuron({len(self.w)})"
   
   def parameters(self):
     return self.w + [self.b]
   
 class Layer:
-  def __init__(self, nin, nout):
-    self.neurons = [Neuron(nin) for _ in range(nout)]
+  def __init__(self, nin, nout, **kwargs):
+    self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]
 
   def __call__(self, x):
     outs = [n(x) for n in self.neurons]
     return outs[0] if len(outs) == 1 else outs
+  
+  def __repr__(self):
+    return f"Layer of [{' '.join(str(neuron) for neuron in self.neurons)}]"
   
   def parameters(self):
     params = []
@@ -33,12 +40,15 @@ class Layer:
 class MLP:
   def __init__(self, nin, nouts):
     sz = [nin] + nouts
-    self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+    self.layers = [Layer(sz[i], sz[i+1], linear=True) for i in range(len(nouts))]
 
   def __call__(self, x):
     for layer in self.layers:
       x = layer(x)
     return x
+  
+  def __repr__(self):
+    return f"MLP of [{' '.join(str(layer) for layer in self.layers)}]"
   
   def parameters(self):
     params = []
