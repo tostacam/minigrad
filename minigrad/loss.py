564f1b2a4172523f9665f11ebbf01a7fb20c7a11
tostacam <edgar-tostado@hotmail.com>
2025-09-19 16:53:20 -0700

    loss.py -> adding classes for MCE, BCE & CE losses

---
+++ minigrad/loss.py
@@ -0,0 +1,28 @@
+from .engine import Value
+
+class MSELoss():
+  def __init__(self):
+    return 
+
+  def __call__(self, y_pred, y_target):
+    loss = sum([(p - t)**2 for p, t in zip(y_pred, y_target)])
+
+    return loss
+  
+class BCELoss():
+  def __init__(self):
+    return
+  
+  def __call__(self, y_pred, y_target):
+    loss = -sum([p.log() if t == 1 else 0 for p, t in zip(y_pred, y_target)])
+
+    return loss
+  
+class CELoss():
+  def __init__(self):
+    return
+
+  def __call__(self, y_pred, y_target):
+    loss = -sum([p.log() if t == 1 else 0 for p, t in zip(y_pred, y_target)])
+
+    return loss
