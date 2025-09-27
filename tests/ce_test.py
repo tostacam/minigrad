cd6212b02ca174f098aad2650f0dc0b567361503
tostacam <edgar-tostado@hotmail.com>
2025-09-26 21:46:40 -0700

    adding separate cross entropy test for debugging

---
+++ tests/ce_test.py
@@ -0,0 +1,26 @@
+import minigrad as mg
+
+# data set
+xs = [1.0, 2.0]
+ys = [1.0, 0.0, 0.0]
+
+# initializing nn and loss
+mg_mlp = mg.MLP(2, [3, 3, 3], ['linear', 'linear', 'softmax'])
+loss_fn = mg.CELoss()
+
+for k in range(20):
+  # forward
+  y_pred = mg_mlp([mg.engine.Value(x) for x in xs])
+  loss = loss_fn(y_pred, ys)
+
+  # backward
+  mg_mlp.zero_grad()
+  loss.backward()
+
+  # udpate params
+  for p in mg_mlp.parameters():
+    p.data += - 0.01 * p.grad
+
+  print(k, loss.data)
+
+mg_mlp.print_nn()
